b7b5a7324aebbba6731cd34f6788122a83574395
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-07-02 20:44:36 +0700

    Use global overworld biomes instead of fabric tag

--- fabric/src/main/java/com/stevekung/fishofthieves/fabric/datagen/FOTDataGeneratorEntrypoint.java
+++ fabric/src/main/java/com/stevekung/fishofthieves/fabric/datagen/FOTDataGeneratorEntrypoint.java
@@ -19,7 +19,6 @@ import com.stevekung.fishofthieves.trigger.ItemUsedOnBlockWithNearbyEntityTrigge
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.*;
-import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
 import net.minecraft.Util;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementRewards;
@@ -389,8 +388,8 @@ public class FOTDataGeneratorEntrypoint implements DataGeneratorEntrypoint
             this.getOrCreateTagBuilder(FOTTags.SPAWNS_ANCIENTSCALES).add(Biomes.LUKEWARM_OCEAN, Biomes.DEEP_LUKEWARM_OCEAN);
             this.getOrCreateTagBuilder(FOTTags.SPAWNS_PLENTIFINS).add(Biomes.WARM_OCEAN, Biomes.LUKEWARM_OCEAN, Biomes.DEEP_LUKEWARM_OCEAN, Biomes.LUSH_CAVES, Biomes.DRIPSTONE_CAVES);
             this.getOrCreateTagBuilder(FOTTags.SPAWNS_WILDSPLASH).forceAddTag(BiomeTags.IS_OCEAN).forceAddTag(BiomeTags.IS_BEACH).forceAddTag(BiomeTags.IS_JUNGLE).add(Biomes.SWAMP, Biomes.LUSH_CAVES, Biomes.WARM_OCEAN);
-            this.getOrCreateTagBuilder(FOTTags.SPAWNS_DEVILFISH).forceAddTag(ConventionalBiomeTags.IN_OVERWORLD);
-            this.getOrCreateTagBuilder(FOTTags.SPAWNS_BATTLEGILLS).forceAddTag(ConventionalBiomeTags.IN_OVERWORLD);
+            this.addOverworldBiomesToTag(FOTTags.SPAWNS_DEVILFISH);
+            this.addOverworldBiomesToTag(FOTTags.SPAWNS_BATTLEGILLS);
             this.getOrCreateTagBuilder(FOTTags.SPAWNS_WRECKERS).forceAddTag(BiomeTags.IS_OCEAN);
             this.getOrCreateTagBuilder(FOTTags.SPAWNS_STORMFISH).forceAddTag(BiomeTags.IS_OCEAN).add(Biomes.SPARSE_JUNGLE);
 
@@ -401,6 +400,32 @@ public class FOTDataGeneratorEntrypoint implements DataGeneratorEntrypoint
         protected Path getPath(ResourceLocation id)
         {
             return this.generator.getOutputFolder().resolve("data/%s/%s/%s.json".formatted(id.getNamespace(), "tags/worldgen/biome", id.getPath()));
+        }
+
+        private void addOverworldBiomesToTag(TagKey<Biome> tag)
+        {
+            this.getOrCreateTagBuilder(tag)
+                    .add(Biomes.RIVER).add(Biomes.FROZEN_RIVER)
+                    .add(Biomes.COLD_OCEAN).add(Biomes.DEEP_COLD_OCEAN)
+                    .add(Biomes.DEEP_FROZEN_OCEAN).add(Biomes.DEEP_OCEAN)
+                    .add(Biomes.DEEP_LUKEWARM_OCEAN).add(Biomes.WARM_OCEAN).add(Biomes.LUKEWARM_OCEAN)
+                    .add(Biomes.FROZEN_OCEAN).add(Biomes.OCEAN)
+                    .add(Biomes.BEACH).add(Biomes.PLAINS)
+                    .add(Biomes.SUNFLOWER_PLAINS).add(Biomes.SNOWY_PLAINS)
+                    .add(Biomes.ICE_SPIKES).add(Biomes.DESERT)
+                    .add(Biomes.FOREST).add(Biomes.FLOWER_FOREST)
+                    .add(Biomes.BIRCH_FOREST).add(Biomes.DARK_FOREST)
+                    .add(Biomes.OLD_GROWTH_BIRCH_FOREST).add(Biomes.OLD_GROWTH_PINE_TAIGA)
+                    .add(Biomes.OLD_GROWTH_SPRUCE_TAIGA).add(Biomes.TAIGA)
+                    .add(Biomes.SNOWY_TAIGA).add(Biomes.SAVANNA)
+                    .add(Biomes.SAVANNA_PLATEAU).add(Biomes.WINDSWEPT_HILLS)
+                    .add(Biomes.WINDSWEPT_GRAVELLY_HILLS).add(Biomes.WINDSWEPT_FOREST)
+                    .add(Biomes.WINDSWEPT_SAVANNA).add(Biomes.JUNGLE).add(Biomes.SPARSE_JUNGLE)
+                    .add(Biomes.BAMBOO_JUNGLE).add(Biomes.BADLANDS).add(Biomes.ERODED_BADLANDS)
+                    .add(Biomes.WOODED_BADLANDS).add(Biomes.MEADOW).add(Biomes.GROVE)
+                    .add(Biomes.SNOWY_SLOPES).add(Biomes.FROZEN_PEAKS).add(Biomes.JAGGED_PEAKS)
+                    .add(Biomes.STONY_PEAKS).add(Biomes.MUSHROOM_FIELDS).add(Biomes.DRIPSTONE_CAVES)
+                    .add(Biomes.LUSH_CAVES).add(Biomes.SNOWY_BEACH).add(Biomes.SWAMP).add(Biomes.STONY_SHORE);
         }
     }
 
