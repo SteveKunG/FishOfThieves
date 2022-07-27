5eff7559d8bb3a65849755f0681fb81ea0be991a
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-07-28 03:01:16 +0700

    Fortune now apply to worms drop, Remove drop rate configs

--- common/src/main/java/com/stevekung/fishofthieves/loot/FOTLootManager.java
+++ common/src/main/java/com/stevekung/fishofthieves/loot/FOTLootManager.java
@@ -2,7 +2,6 @@ package com.stevekung.fishofthieves.loot;
 
 import java.util.List;
 
-import com.stevekung.fishofthieves.core.FishOfThieves;
 import com.stevekung.fishofthieves.predicates.FOTLocationCheck;
 import com.stevekung.fishofthieves.predicates.FOTLocationPredicate;
 import com.stevekung.fishofthieves.registry.FOTItems;
@@ -14,18 +13,20 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.data.loot.BlockLoot;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.BonusLevelTableCondition;
 import net.minecraft.world.level.storage.loot.predicates.LocationCheck;
 
 public class FOTLootManager
 {
     public static void dropWorms(List<ItemStack> droppedList, BlockState blockState, LootContext lootContext)
     {
-        var random = lootContext.getRandom();
         var waterPredicate = LocationPredicate.Builder.location().setFluid(FluidPredicate.Builder.fluid().of(FluidTags.WATER).build());
         var noSilkTouch = BlockLoot.HAS_NO_SILK_TOUCH.build().test(lootContext);
+        var shouldDrop = BonusLevelTableCondition.bonusLevelFlatChance(Enchantments.BLOCK_FORTUNE, 0.1f, 0.14285715f, 0.25f, 0.5f).build().test(lootContext);
         var waterSurrounded = LocationCheck.checkLocation(waterPredicate, new BlockPos(1, 0, 0))
                 .or(LocationCheck.checkLocation(waterPredicate, new BlockPos(-1, 0, 0)))
                 .or(LocationCheck.checkLocation(waterPredicate, new BlockPos(0, 0, 1)))
@@ -39,15 +40,15 @@ public class FOTLootManager
             return;
         }
 
-        if (random.nextFloat() < FishOfThieves.CONFIG.dropRate.earthwormsDropRate && blockState.is(FOTTags.EARTHWORMS_DROPS))
+        if (shouldDrop && blockState.is(FOTTags.EARTHWORMS_DROPS))
         {
             droppedList.add(new ItemStack(FOTItems.EARTHWORMS));
         }
-        if (random.nextFloat() < FishOfThieves.CONFIG.dropRate.grubsDropRate && blockState.is(FOTTags.GRUBS_DROPS) && !waterSurrounded)
+        if (shouldDrop && blockState.is(FOTTags.GRUBS_DROPS) && !waterSurrounded)
         {
             droppedList.add(new ItemStack(FOTItems.GRUBS));
         }
-        if (random.nextFloat() < FishOfThieves.CONFIG.dropRate.leechesDropRate && blockState.is(FOTTags.LEECHES_DROPS) && shouldDropLeeches)
+        if (shouldDrop && blockState.is(FOTTags.LEECHES_DROPS) && shouldDropLeeches)
         {
             droppedList.add(new ItemStack(FOTItems.LEECHES));
         }
