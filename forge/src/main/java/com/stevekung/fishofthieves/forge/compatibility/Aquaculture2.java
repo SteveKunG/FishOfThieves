6514b6700d1aa7d586f4ae0307d24422d6a20004
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-07-28 13:09:39 +0700

    Aquaculture fish fillet balanced

--- forge/src/main/java/com/stevekung/fishofthieves/forge/compatibility/Aquaculture2.java
+++ forge/src/main/java/com/stevekung/fishofthieves/forge/compatibility/Aquaculture2.java
@@ -7,15 +7,15 @@ public class Aquaculture2
 {
     public static void init()
     {
-        AquacultureAPI.FISH_DATA.add(FOTItems.SPLASHTAIL, 1.5, 50, 5);
-        AquacultureAPI.FISH_DATA.add(FOTItems.PONDIE, 1, 50, 5);
-        AquacultureAPI.FISH_DATA.add(FOTItems.ISLEHOPPER, 1, 75, 8);
-        AquacultureAPI.FISH_DATA.add(FOTItems.ANCIENTSCALE, 2, 75, 8);
-        AquacultureAPI.FISH_DATA.add(FOTItems.PLENTIFIN, 1, 50, 5);
-        AquacultureAPI.FISH_DATA.add(FOTItems.WILDSPLASH, 1, 100, 8);
-        AquacultureAPI.FISH_DATA.add(FOTItems.DEVILFISH, 1, 150, 12);
-        AquacultureAPI.FISH_DATA.add(FOTItems.BATTLEGILL, 1, 150, 12);
-        AquacultureAPI.FISH_DATA.add(FOTItems.WRECKER, 1, 180, 15);
-        AquacultureAPI.FISH_DATA.add(FOTItems.STORMFISH, 1, 200, 20);
+        AquacultureAPI.FISH_DATA.add(FOTItems.SPLASHTAIL, 20, 50, 5);
+        AquacultureAPI.FISH_DATA.add(FOTItems.PONDIE, 10, 40, 3);
+        AquacultureAPI.FISH_DATA.add(FOTItems.ISLEHOPPER, 20, 75, 5);
+        AquacultureAPI.FISH_DATA.add(FOTItems.ANCIENTSCALE, 15, 50, 4);
+        AquacultureAPI.FISH_DATA.add(FOTItems.PLENTIFIN, 20, 50, 4);
+        AquacultureAPI.FISH_DATA.add(FOTItems.WILDSPLASH, 15, 80, 5);
+        AquacultureAPI.FISH_DATA.add(FOTItems.DEVILFISH, 20, 80, 6);
+        AquacultureAPI.FISH_DATA.add(FOTItems.BATTLEGILL, 20, 80, 6);
+        AquacultureAPI.FISH_DATA.add(FOTItems.WRECKER, 30, 100, 8);
+        AquacultureAPI.FISH_DATA.add(FOTItems.STORMFISH, 30, 120, 10);
     }
 }
