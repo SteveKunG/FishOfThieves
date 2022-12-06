4d8256857027c583d1c9cf3bf64dec3447d6778c
Wasinthorn Suksri <mccommander_minecraft@hotmail.com>
2022-12-06 12:22:57 +0700

    Added DataFixer for missing recipe advancements

---
+++ common/src/main/java/com/stevekung/fishofthieves/mixin/datafix/MixinDataFixers.java
@@ -0,0 +1,52 @@
+package com.stevekung.fishofthieves.mixin.datafix;
+
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import com.google.common.collect.ImmutableMap;
+import com.mojang.datafixers.DataFixerBuilder;
+import com.mojang.datafixers.schemas.Schema;
+import net.minecraft.util.datafix.DataFixers;
+import net.minecraft.util.datafix.fixes.AdvancementsRenameFix;
+
+@Mixin(DataFixers.class)
+public class MixinDataFixers
+{
+    @Shadow
+    @Final
+    static BiFunction<Integer, Schema, Schema> SAME_NAMESPACED;
+
+    @Shadow
+    static UnaryOperator<String> createRenamer(Map<String, String> renameMap)
+    {
+        throw new AssertionError();
+    }
+
+    @Inject(method = "addFixers", at = @At("TAIL"))
+    private static void fishofthieves$addFixers(DataFixerBuilder builder, CallbackInfo info)
+    {
+        var schema = builder.addSchema(3210, SAME_NAMESPACED);
+
+        //@formatter:off
+        builder.addFixer(new AdvancementsRenameFix(schema, false, "Rename FOT recipe advancements", createRenamer(ImmutableMap.<String, String>builder()
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_splashtail", "fishofthieves:recipes/food/cooked_splashtail")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_pondie", "fishofthieves:recipes/food/cooked_pondie")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_islehopper", "fishofthieves:recipes/food/cooked_islehopper")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_plentifin", "fishofthieves:recipes/food/cooked_plentifin")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_ancientscale", "fishofthieves:recipes/food/cooked_ancientscale")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_wildsplash", "fishofthieves:recipes/food/cooked_wildsplash")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_devilfish", "fishofthieves:recipes/food/cooked_devilfish")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_battlegill", "fishofthieves:recipes/food/cooked_battlegill")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_wrecker", "fishofthieves:recipes/food/cooked_wrecker")
+                .put("fishofthieves:recipes/fishofthieves.main/cooked_stormfish", "fishofthieves:recipes/food/cooked_stormfish")
+                .build())));
+        //@formatter:on
+    }
+}
