d6a5d225f95d3dc0daf92fe0c30edec5d1fde254
SteveKunG <mccommander_minecraft@hotmail.com>
2024-05-27 16:43:35 +0700

    Reenable wrecker bulb layer

--- common/src/main/java/com/stevekung/fishofthieves/client/renderer/entity/layers/WreckerBulbLayer.java
+++ common/src/main/java/com/stevekung/fishofthieves/client/renderer/entity/layers/WreckerBulbLayer.java
@@ -1,27 +1,24 @@
 package com.stevekung.fishofthieves.client.renderer.entity.layers;
 
-import java.util.Map;
 import java.util.function.Function;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.stevekung.fishofthieves.FishOfThieves;
 import com.stevekung.fishofthieves.entity.animal.Wrecker;
 import com.stevekung.fishofthieves.entity.variant.WreckerVariant;
-import net.minecraft.Util;
+import com.stevekung.fishofthieves.registry.variant.WreckerVariants;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 
 public class WreckerBulbLayer<T extends Wrecker, M extends EntityModel<T>> extends RenderLayer<T, M>
 {
-//    private static final Map<WreckerVariant, ResourceLocation> BULB_BY_TYPE = Util.make(Maps.newHashMap(), map -> map.putAll(BBFOTBuiltInRegistries.WRECKER_VARIANT.stream().collect(ImmutableMap.toImmutableMap(Function.identity(), variant -> FishOfThieves.res("textures/entity/wrecker/%s_bulb.png".formatted(BBFOTBuiltInRegistries.WRECKER_VARIANT.getKey(variant).getPath()))))));
+    private static final Function<Holder<WreckerVariant>, ResourceLocation> BULB_BY_VARIANT = variant -> new ResourceLocation("textures/entity/wrecker/%s_bulb.png".formatted(variant.unwrapKey().orElse(WreckerVariants.ROSE).location().getPath()));
 
     public WreckerBulbLayer(RenderLayerParent<T, M> renderLayerParent)
     {
@@ -31,11 +28,11 @@ public class WreckerBulbLayer<T extends Wrecker, M extends EntityModel<T>> exten
     @Override
     public void render(PoseStack poseStack, MultiBufferSource buffer, int packedLight, T livingEntity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch)
     {
-//        if (!livingEntity.isInvisible())
-//        {
-//            var vertexConsumer = buffer.getBuffer(RenderType.eyes(BULB_BY_TYPE.get(livingEntity.getVariant().value())));
-//            var color = Mth.clamp(1.0F + Mth.cos(ageInTicks * 0.05f), 0.25F, 1.0F);
-//            this.getParentModel().renderToBuffer(poseStack, vertexConsumer, 15728640, OverlayTexture.NO_OVERLAY, color, color, color, 1.0f);
-//        }
+        if (!livingEntity.isInvisible())
+        {
+            var vertexConsumer = buffer.getBuffer(RenderType.eyes(BULB_BY_VARIANT.apply(livingEntity.getVariant())));
+            var color = Mth.clamp(1.0F + Mth.cos(ageInTicks * 0.05f), 0.25F, 1.0F);
+            this.getParentModel().renderToBuffer(poseStack, vertexConsumer, 15728640, OverlayTexture.NO_OVERLAY, color, color, color, 1.0f);
+        }
     }
 }
