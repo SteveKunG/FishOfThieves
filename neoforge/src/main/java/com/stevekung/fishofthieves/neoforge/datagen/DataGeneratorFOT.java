3071ffac5b33ea85ab1fa1225881805489c44ae1
SteveKunG <mccommander_minecraft@hotmail.com>
2024-06-22 09:18:32 +0700

    Remove old forge datagen

--- neoforge/src/main/java/com/stevekung/fishofthieves/neoforge/datagen/DataGeneratorFOT.java
+++ neoforge/src/main/java/com/stevekung/fishofthieves/neoforge/datagen/DataGeneratorFOT.java
@@ -12,55 +12,27 @@ import com.stevekung.fishofthieves.registry.FOTEntities;
 import com.stevekung.fishofthieves.registry.FOTItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.data.tags.VanillaBlockTagsProvider;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 @EventBusSubscriber(modid = FishOfThieves.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class DataGeneratorFOT
 {
-    private static final TagKey<Item> RAW_FISHES = TagKey.create(Registries.ITEM, new ResourceLocation("forge", "raw_fishes"));
-    private static final TagKey<Item> COOKED_FISHES = TagKey.create(Registries.ITEM, new ResourceLocation("forge", "cooked_fishes"));
-
     @SubscribeEvent
     public static void onGatherData(GatherDataEvent event)
     {
         var dataGenerator = event.getGenerator();
         var packOutput = dataGenerator.getPackOutput();
         var provider = event.getLookupProvider();
-        var helper = event.getExistingFileHelper();
-        dataGenerator.addProvider(event.includeServer(), new ForgeItemTags(packOutput, provider, FishOfThieves.MOD_ID, helper));
         dataGenerator.addProvider(event.includeServer(), new FishingReal(packOutput, provider));
-    }
-
-    private static class ForgeItemTags extends ItemTagsProvider
-    {
-        public ForgeItemTags(PackOutput output, CompletableFuture<HolderLookup.Provider> provider, String modId, @Nullable ExistingFileHelper existingFileHelper)
-        {
-            super(output, provider, new VanillaBlockTagsProvider(output, provider).contentsGetter(), modId, existingFileHelper);
-        }
-
-        @Override
-        protected void addTags(HolderLookup.Provider provider)
-        {
-            var rawFishes = new Item[] { FOTItems.SPLASHTAIL, FOTItems.PONDIE, FOTItems.ISLEHOPPER, FOTItems.ANCIENTSCALE, FOTItems.PLENTIFIN, FOTItems.WILDSPLASH, FOTItems.DEVILFISH, FOTItems.BATTLEGILL, FOTItems.WRECKER, FOTItems.STORMFISH };
-            var cookedFishes = new Item[] { FOTItems.COOKED_SPLASHTAIL, FOTItems.COOKED_PONDIE, FOTItems.COOKED_ISLEHOPPER, FOTItems.COOKED_ANCIENTSCALE, FOTItems.COOKED_PLENTIFIN, FOTItems.COOKED_WILDSPLASH, FOTItems.COOKED_DEVILFISH, FOTItems.COOKED_BATTLEGILL, FOTItems.COOKED_WRECKER, FOTItems.COOKED_STORMFISH };
-
-            this.tag(RAW_FISHES).add(rawFishes);
-            this.tag(COOKED_FISHES).add(cookedFishes);
-        }
     }
 
     private static class FishingReal extends FishingRealProvider
