dc52bfc5b0c0c0b1971de4c4ca2b53cb506873dc
SteveKunG <mccommander_minecraft@hotmail.com>
2024-07-09 10:48:04 +0700

    Added support for FishingReal for both platforms

--- forge/src/main/java/com/stevekung/fishofthieves/forge/datagen/DataGeneratorFOT.java
+++ forge/src/main/java/com/stevekung/fishofthieves/forge/datagen/DataGeneratorFOT.java
@@ -1,32 +1,22 @@
 package com.stevekung.fishofthieves.forge.datagen;
 
-import java.nio.file.Path;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.jetbrains.annotations.Nullable;
-import com.google.common.collect.Maps;
-import com.google.gson.JsonObject;
 import com.stevekung.fishofthieves.FishOfThieves;
-import com.stevekung.fishofthieves.registry.FOTEntities;
 import com.stevekung.fishofthieves.registry.FOTItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.CachedOutput;
-import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.data.tags.VanillaBlockTagsProvider;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = FishOfThieves.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DataGeneratorFOT
@@ -42,7 +32,6 @@ public class DataGeneratorFOT
         var provider = event.getLookupProvider();
         var helper = event.getExistingFileHelper();
         dataGenerator.addProvider(event.includeServer(), new ForgeItemTags(packOutput, provider, FishOfThieves.MOD_ID, helper));
-        dataGenerator.addProvider(event.includeServer(), new FishingReal(packOutput, provider));
     }
 
     private static class ForgeItemTags extends ItemTagsProvider
@@ -60,104 +49,6 @@ public class DataGeneratorFOT
 
             this.tag(RAW_FISHES).add(rawFishes);
             this.tag(COOKED_FISHES).add(cookedFishes);
-        }
-    }
-
-    private static class FishingReal extends FishingRealProvider
-    {
-        public FishingReal(PackOutput output, CompletableFuture<HolderLookup.Provider> provider)
-        {
-            super(output, provider);
-        }
-
-        @Override
-        public void addFishingReal()
-        {
-            this.add(FOTItems.SPLASHTAIL, FOTEntities.SPLASHTAIL);
-            this.add(FOTItems.PONDIE, FOTEntities.PONDIE);
-            this.add(FOTItems.ISLEHOPPER, FOTEntities.ISLEHOPPER);
-            this.add(FOTItems.ANCIENTSCALE, FOTEntities.ANCIENTSCALE);
-            this.add(FOTItems.PLENTIFIN, FOTEntities.PLENTIFIN);
-            this.add(FOTItems.WILDSPLASH, FOTEntities.WILDSPLASH);
-            this.add(FOTItems.DEVILFISH, FOTEntities.DEVILFISH);
-            this.add(FOTItems.BATTLEGILL, FOTEntities.BATTLEGILL);
-            this.add(FOTItems.WRECKER, FOTEntities.WRECKER);
-            this.add(FOTItems.STORMFISH, FOTEntities.STORMFISH);
-        }
-    }
-
-    private static abstract class FishingRealProvider implements DataProvider
-    {
-        private final Map<ResourceLocation, FishingRealBuilder> builders = Maps.newLinkedHashMap();
-        private final PackOutput output;
-        private final CompletableFuture<HolderLookup.Provider> provider;
-
-        public FishingRealProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> provider)
-        {
-            this.output = output;
-            this.provider = provider;
-        }
-
-        public abstract void addFishingReal();
-
-        public void add(Item item, EntityType<?> entityType)
-        {
-            this.add(item, entityType, null);
-        }
-
-        public void add(Item item, EntityType<?> entityType, @Nullable CompoundTag compoundTag)
-        {
-            var builder = new FishingRealBuilder(item, entityType, compoundTag);
-            this.builders.put(ForgeRegistries.ENTITY_TYPES.getKey(entityType), builder);
-        }
-
-        @Override
-        public CompletableFuture<?> run(CachedOutput cachedOutput)
-        {
-            return this.provider.thenCompose(provider ->
-            {
-                this.builders.clear();
-                this.addFishingReal();
-
-                return CompletableFuture.allOf(this.builders.entrySet().stream().map(entry ->
-                {
-                    var jsonObject = entry.getValue().serializeToJson();
-                    var path = this.getPath(entry.getKey());
-                    return DataProvider.saveStable(cachedOutput, jsonObject, path);
-                }).toArray(CompletableFuture[]::new));
-            });
-        }
-
-        @Override
-        public String getName()
-        {
-            return "FishingReal";
-        }
-
-        private Path getPath(ResourceLocation id)
-        {
-            return this.output.getOutputFolder().resolve("data/fishingreal/fishing/" + id.getPath() + ".json");
-        }
-
-        record FishingRealBuilder(Item item, EntityType<?> entityType, @Nullable CompoundTag compoundTag)
-        {
-            public JsonObject serializeToJson()
-            {
-                var jsonObject = new JsonObject();
-                var jsonItem = new JsonObject();
-                var jsonResult = new JsonObject();
-                jsonItem.addProperty("item", ForgeRegistries.ITEMS.getKey(this.item).toString());
-                jsonResult.addProperty("id", ForgeRegistries.ENTITY_TYPES.getKey(this.entityType).toString());
-
-                if (this.compoundTag != null)
-                {
-                    jsonResult.addProperty("nbt", this.compoundTag.toString());
-                }
-
-                jsonObject.add("input", jsonItem);
-                jsonObject.add("result", jsonResult);
-                return jsonObject;
-            }
         }
     }
 }
