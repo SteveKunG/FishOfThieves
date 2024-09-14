8f5fe5b1933be91d6b5e899454a712e0904951cd
SteveKunG <mccommander_minecraft@hotmail.com>
2024-09-15 00:51:02 +0700

    Added bottom state for banana stem

--- common/src/main/java/com/stevekung/fishofthieves/block/BananaStemBlock.java
+++ common/src/main/java/com/stevekung/fishofthieves/block/BananaStemBlock.java
@@ -3,17 +3,23 @@ package com.stevekung.fishofthieves.block;
 import com.stevekung.fishofthieves.registry.FOTTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.BonemealableBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class BananaStemBlock extends SmallRotatedPillarBlock
+public class BananaStemBlock extends SmallRotatedPillarBlock implements BonemealableBlock
 {
     public static final BooleanProperty TOP = BooleanProperty.create("top");
+    public static final BooleanProperty BOTTOM = BooleanProperty.create("bottom");
     private static final VoxelShape SHAPE_VERTICAL = Block.box(3, 0, 3, 13, 16, 13);
     private static final VoxelShape SHAPE_HORIZONTAL_NS = Block.box(3, 3, 0, 13, 13, 16);
     private static final VoxelShape SHAPE_HORIZONTAL_WE = Block.box(0, 3, 3, 16, 13, 13);
@@ -21,7 +27,31 @@ public class BananaStemBlock extends SmallRotatedPillarBlock
     public BananaStemBlock(Properties properties)
     {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y).setValue(WATERLOGGED, false).setValue(TOP, false));
+        this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y).setValue(WATERLOGGED, false).setValue(BOTTOM, false).setValue(TOP, false));
+    }
+
+    @Override
+    public boolean isValidBonemealTarget(LevelReader level, BlockPos pos, BlockState state, boolean isClient)
+    {
+        return state.getValue(BOTTOM) && Direction.Plane.HORIZONTAL.stream().anyMatch(direction -> level.getBlockState(pos.relative(direction)).canBeReplaced());
+    }
+
+    @Override
+    public boolean isBonemealSuccess(Level level, RandomSource random, BlockPos pos, BlockState state)
+    {
+        return true;
+    }
+
+    @Override
+    public void performBonemeal(ServerLevel level, RandomSource random, BlockPos pos, BlockState state)
+    {
+        var direction = Direction.Plane.HORIZONTAL.getRandomDirection(random);
+        var blockState = level.getBlockState(pos.relative(direction));
+
+        if (blockState.isAir())
+        {
+//            level.setBlock(pos.relative(direction), FOTBlocks.COCONUT_FRUIT.defaultBlockState().setValue(CoconutFruitBlock.FACING, direction.getOpposite()), Block.UPDATE_CLIENTS);
+        }
     }
 
     @Override
@@ -53,6 +83,6 @@ public class BananaStemBlock extends SmallRotatedPillarBlock
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
-        super.createBlockStateDefinition(builder.add(TOP));
+        super.createBlockStateDefinition(builder.add(TOP, BOTTOM));
     }
 }
