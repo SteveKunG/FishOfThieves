26a8b3ba52d94b1335971b89f4e0e75cbb185299
SteveKunG <mccommander_minecraft@hotmail.com>
2024-10-04 16:29:16 +0700

    Update coconut fruit and coconut sapling textures and model

--- common/src/main/java/com/stevekung/fishofthieves/block/CoconutSaplingBlock.java
+++ common/src/main/java/com/stevekung/fishofthieves/block/CoconutSaplingBlock.java
@@ -1,19 +1,51 @@
 package com.stevekung.fishofthieves.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.monster.Ravager;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.GameRules;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
+@SuppressWarnings("deprecation")
 public class CoconutSaplingBlock extends SaplingBlock
 {
+    private static final VoxelShape AABB = Block.box(3, 0, 3, 13, 5, 13);
+
     public CoconutSaplingBlock(AbstractTreeGrower treeGrower, BlockBehaviour.Properties properties)
     {
         super(treeGrower, properties);
         this.registerDefaultState(this.stateDefinition.any().setValue(STAGE, 0));
+    }
+
+    @Override
+    public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity)
+    {
+        if (entity instanceof Ravager && level.getGameRules().getBoolean(GameRules.RULE_MOBGRIEFING))
+        {
+            level.destroyBlock(pos, true, entity);
+        }
+        super.entityInside(state, level, pos, entity);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
+    {
+        return AABB;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context)
+    {
+        return AABB;
     }
 
     @Override
