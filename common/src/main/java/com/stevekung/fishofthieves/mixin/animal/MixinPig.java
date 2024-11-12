ad5e6874ba76797a34ba7e1173fbf2ca50ce85d6
SteveKunG <mccommander_minecraft@hotmail.com>
2024-11-12 17:56:37 +0700

    Banana and Banana Blossom can be used to breed pig

---
+++ common/src/main/java/com/stevekung/fishofthieves/mixin/animal/MixinPig.java
@@ -0,0 +1,29 @@
+package com.stevekung.fishofthieves.mixin.animal;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import com.google.common.collect.ObjectArrays;
+import com.stevekung.fishofthieves.registry.FOTBlocks;
+import com.stevekung.fishofthieves.registry.FOTItems;
+import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+
+@Mixin(Pig.class)
+public class MixinPig
+{
+    @Shadow
+    @Final
+    @Mutable
+    static Ingredient FOOD_ITEMS;
+
+    @ModifyArg(method = "registerGoals", at = @At(value = "INVOKE", target = "net/minecraft/world/entity/ai/goal/TemptGoal.<init>(Lnet/minecraft/world/entity/PathfinderMob;DLnet/minecraft/world/item/crafting/Ingredient;Z)V"), index = 2)
+    private Ingredient fishofthieves$addNewTempt(Ingredient original)
+    {
+        return FOOD_ITEMS = Ingredient.of(ObjectArrays.concat(FOOD_ITEMS.getItems(), Ingredient.of(FOTItems.BANANA, FOTBlocks.BANANA_BLOSSOM).getItems(), ItemStack.class));
+    }
+}
