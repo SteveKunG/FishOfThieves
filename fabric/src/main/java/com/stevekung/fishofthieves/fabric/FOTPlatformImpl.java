98b19f332562148721632cbfa7d6e9b0b3b04e8f
SteveKunG <mccommander_minecraft@hotmail.com>
2025-03-11 22:55:00 +0700

    Cleanup unused imports

--- fabric/src/main/java/com/stevekung/fishofthieves/fabric/FOTPlatformImpl.java
+++ fabric/src/main/java/com/stevekung/fishofthieves/fabric/FOTPlatformImpl.java
@@ -1,18 +1,10 @@
 package com.stevekung.fishofthieves.fabric;
 
-import com.stevekung.fishofthieves.FishOfThieves;
-
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityDimensions;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.CropBlock;
