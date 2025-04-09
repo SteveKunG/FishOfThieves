7db2724f836dec6a10cf02141fcd4dc63e53b954
SteveKunG <mccommander_minecraft@hotmail.com>
2025-04-09 22:35:23 +0700

    Add datafix for coconut boat and chest boat

---
+++ common/src/main/java/com/stevekung/fishofthieves/mixin/datafix/MixinBoatSplitFix.java
@@ -0,0 +1,30 @@
+package com.stevekung.fishofthieves.mixin.datafix;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.util.datafix.fixes.BoatSplitFix;
+
+@Mixin(BoatSplitFix.class)
+public class MixinBoatSplitFix
+{
+    @Inject(method = "mapVariantToNormalBoat", cancellable = true, at = @At("HEAD"))
+    private static void fishofthieves$mapNormalBoat(String variant, CallbackInfoReturnable<String> info)
+    {
+        if (variant.equals("coconut"))
+        {
+            info.setReturnValue("fishofthieves:coconut_boat");
+        }
+    }
+
+    @Inject(method = "mapVariantToChestBoat", cancellable = true, at = @At("HEAD"))
+    private static void fishofthieves$mapChestBoat(String variant, CallbackInfoReturnable<String> info)
+    {
+        if (variant.equals("coconut"))
+        {
+            info.setReturnValue("fishofthieves:coconut_chest_boat");
+        }
+    }
+}
