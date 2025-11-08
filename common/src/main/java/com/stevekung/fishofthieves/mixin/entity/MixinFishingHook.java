b24958b36e38ed2828e63151f3477493861d62f3
SteveKunG <mccommander_minecraft@hotmail.com>
2025-11-08 16:39:42 +0700

    feat: worms are now increased fishing speed by 10 percent

--- common/src/main/java/com/stevekung/fishofthieves/mixin/entity/MixinFishingHook.java
+++ common/src/main/java/com/stevekung/fishofthieves/mixin/entity/MixinFishingHook.java
@@ -1,20 +1,24 @@
 package com.stevekung.fishofthieves.mixin.entity;
 
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.stevekung.fishofthieves.FOTPlatform;
 import com.stevekung.fishofthieves.entity.FishingHookBait;
+import com.stevekung.fishofthieves.registry.FOTTags;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.stats.Stats;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.FishingHook;
@@ -25,6 +29,9 @@ import net.minecraft.world.phys.Vec3;
 @Mixin(FishingHook.class)
 public abstract class MixinFishingHook extends Projectile implements FishingHookBait
 {
+    @Shadow
+    int timeUntilLured;
+
     @Unique
     private ItemStack baitStack = ItemStack.EMPTY;
 
@@ -86,6 +93,29 @@ public abstract class MixinFishingHook extends Projectile implements FishingHook
     private void fishofthieves$shrinkBait(BlockPos pos, CallbackInfo info)
     {
         this.shrinkBait(true);
+    }
+
+    @Inject(
+            method = "catchingFish",
+            at = @At(
+                    value = "FIELD",
+                    target = "net/minecraft/world/entity/projectile/FishingHook.timeUntilLured:I",
+                    opcode = Opcodes.PUTFIELD,
+                    ordinal = 1,
+                    shift = At.Shift.AFTER),
+            slice = @Slice(
+                    from = @At(
+                            value = "INVOKE",
+                            target = "net/minecraft/util/Mth.nextInt(Lnet/minecraft/util/RandomSource;II)I",
+                            ordinal = 1))
+    )
+    private void fishofthieves$increaseLureByBait(BlockPos pos, CallbackInfo info)
+    {
+        if (this.baitStack.is(FOTTags.Items.WORMS))
+        {
+            // Increase lure speed by 10%
+            this.timeUntilLured = Mth.floor(this.timeUntilLured * 0.9);
+        }
     }
 
     @Override
