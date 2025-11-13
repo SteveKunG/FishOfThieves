d58a948ea2b005fbf96d9d20d0452b362728f7e2
SteveKunG <mccommander_minecraft@hotmail.com>
2025-11-13 17:16:00 +0700

    chore: re-enable mod menu

--- fabric/src/main/java/com/stevekung/fishofthieves/fabric/modmenu/ModMenuIntegration.java
+++ fabric/src/main/java/com/stevekung/fishofthieves/fabric/modmenu/ModMenuIntegration.java
@@ -1,10 +1,16 @@
 package com.stevekung.fishofthieves.fabric.modmenu;
 
-//public class ModMenuIntegration implements ModMenuApi TODO
-//{
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory()
-//    {
-//        return parent -> AutoConfig.getConfigScreen(FishOfThievesConfig.class, parent).get();
-//    }
-//}
+import com.stevekung.fishofthieves.config.FishOfThievesConfig;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi
+{
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory()
+    {
+        return parent -> AutoConfig.getConfigScreen(FishOfThievesConfig.class, parent).get();
+    }
+}
