be21658216119c46ea1b262c7eac39f9b1e15662
SteveKunG <mccommander_minecraft@hotmail.com>
2025-11-26 13:59:10 +0700

    feat: implement BucketPickup for shoal block

--- common/src/main/java/com/stevekung/fishofthieves/block/ShoalBlock.java
+++ common/src/main/java/com/stevekung/fishofthieves/block/ShoalBlock.java
@@ -1,16 +1,22 @@
 package com.stevekung.fishofthieves.block;
 
+import java.util.Optional;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.tags.FluidTags;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.BucketPickup;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -23,7 +29,7 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 @SuppressWarnings("deprecation")
-public class ShoalBlock extends Block
+public class ShoalBlock extends Block implements BucketPickup
 {
     public static final BooleanProperty TREASURED = BooleanProperty.create("treasured");
 
@@ -96,6 +102,19 @@ public class ShoalBlock extends Block
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder)
     {
         builder.add(TREASURED);
+    }
+
+    @Override
+    public ItemStack pickupBlock(LevelAccessor level, BlockPos pos, BlockState state)
+    {
+        level.setBlock(pos, Blocks.AIR.defaultBlockState(), Block.UPDATE_ALL_IMMEDIATE);
+        return new ItemStack(Items.WATER_BUCKET);
+    }
+
+    @Override
+    public Optional<SoundEvent> getPickupSound()
+    {
+        return Fluids.WATER.getPickupSound();
     }
 
     private void destroyShoal(BlockState state, Level level, BlockPos pos)
