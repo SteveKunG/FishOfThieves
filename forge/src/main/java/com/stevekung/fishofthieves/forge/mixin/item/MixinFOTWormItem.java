767615dbac0dd53d90140c3f8b68619f4602bafd
SteveKunG <mccommander_minecraft@hotmail.com>
2025-12-03 08:43:20 +0700

    fix: add mixin constraints for aquaculture compatibility

--- forge/src/main/java/com/stevekung/fishofthieves/forge/mixin/item/MixinFOTWormItem.java
+++ forge/src/main/java/com/stevekung/fishofthieves/forge/mixin/item/MixinFOTWormItem.java
@@ -2,11 +2,13 @@ package com.stevekung.fishofthieves.forge.mixin.item;
 
 import org.spongepowered.asm.mixin.Mixin;
 
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
 import com.stevekung.fishofthieves.item.FOTWormItem;
 import com.stevekung.fishofthieves.registry.FOTItems;
 import com.teammetallurgy.aquaculture.api.bait.IBaitItem;
 
 @Mixin(FOTWormItem.class)
+@IfModLoaded(value = "aquaculture")
 public class MixinFOTWormItem implements IBaitItem
 {
     @Override
