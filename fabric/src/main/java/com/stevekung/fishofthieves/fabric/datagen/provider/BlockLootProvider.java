fb496779d1a22462d543028e95e38641f4e461da
SteveKunG <mccommander_minecraft@hotmail.com>
2025-12-15 14:28:10 +0700

    chore: coconunt fronds now has drop chance of 33, 50, 100 percent

--- fabric/src/main/java/com/stevekung/fishofthieves/fabric/datagen/provider/BlockLootProvider.java
+++ fabric/src/main/java/com/stevekung/fishofthieves/fabric/datagen/provider/BlockLootProvider.java
@@ -22,6 +22,7 @@ import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.ApplyBonusCount;
 import net.minecraft.world.level.storage.loot.functions.LimitCount;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.predicates.BonusLevelTableCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
@@ -126,11 +127,11 @@ public class BlockLootProvider extends FabricBlockLootTableProvider
                                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(2f, 4f))))
                         .when(LootItemBlockStatePropertyCondition.hasBlockStateProperties(block)
                                 .setProperties(StatePropertiesPredicate.Builder.properties().hasProperty(CoconutFruitBlock.AGE, 2)))));
-        this.dropSelf(FOTBlocks.COCONUT_FRONDS);
+        this.add(FOTBlocks.COCONUT_FRONDS, block -> createSilkTouchOrShearsDispatchTable(block, LootItem.lootTableItem(block).when(BonusLevelTableCondition.bonusLevelFlatChance(Enchantments.BLOCK_FORTUNE, 0.33F, 0.5F, 1.0F))));
         this.dropSelf(FOTBlocks.BANANA_STEM);
         this.dropOther(FOTBlocks.BANANA_CLUSTER_GROWABLE_STEM, FOTBlocks.BANANA_STEM);
         this.dropOther(FOTBlocks.VERTICAL_BANANA_LEAVES, FOTBlocks.BANANA_LEAVES);
-        this.dropOther(FOTBlocks.VERTICAL_COCONUT_FRONDS, FOTBlocks.COCONUT_FRONDS);
+        this.add(FOTBlocks.VERTICAL_COCONUT_FRONDS, block -> createSilkTouchOrShearsDispatchTable(FOTBlocks.COCONUT_FRONDS, LootItem.lootTableItem(FOTBlocks.COCONUT_FRONDS).when(BonusLevelTableCondition.bonusLevelFlatChance(Enchantments.BLOCK_FORTUNE, 0.33F, 0.5F, 1.0F))));
         this.add(FOTBlocks.BANANA_LEAVES, block -> LootTable.lootTable()
                 .withPool(this.applyExplosionCondition(block, LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(1.0F))
