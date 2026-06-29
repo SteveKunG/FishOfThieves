bfba0b8daacbcc147c3e7ef3c8dbc779c944ff78
SteveKunG <mccommander_minecraft@hotmail.com>
2026-06-29 23:04:30 +0700

    chore: re-enabled terrablender

--- common/src/main/java/com/stevekung/fishofthieves/compatibility/terrablender/FOTTerraBlender.java
+++ common/src/main/java/com/stevekung/fishofthieves/compatibility/terrablender/FOTTerraBlender.java
@@ -1,12 +1,20 @@
 package com.stevekung.fishofthieves.compatibility.terrablender;
 
+import com.stevekung.fishofthieves.FishOfThieves;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.registries.VanillaRegistries;
+
+import terrablender.api.Regions;
+import terrablender.api.SurfaceRuleManager;
+
 public class FOTTerraBlender
 {
     public static void init()
     {
-//        Regions.register(new TropicalIslandRegion(FishOfThieves.id("tropical_island"), FishOfThieves.CONFIG.biome.tropicalIslandRegionWeight));
+        Regions.register(new TropicalIslandRegion(FishOfThieves.id("tropical_island"), FishOfThieves.CONFIG.biome.tropicalIslandRegionWeight));
 
-        //TODO
-//        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, FishOfThieves.MOD_ID, FOTSurfaceRuleData.overworld());
+        var biomes = VanillaRegistries.createLookup().lookupOrThrow(Registries.BIOME);
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, FishOfThieves.MOD_ID, FOTSurfaceRuleData.overworld(biomes));
     }
 }
--- common/src/main/java/com/stevekung/fishofthieves/compatibility/terrablender/TropicalIslandRegion.java
+++ common/src/main/java/com/stevekung/fishofthieves/compatibility/terrablender/TropicalIslandRegion.java
@@ -1,51 +1,51 @@
-//package com.stevekung.fishofthieves.compatibility.terrablender;
-//
-//import java.util.function.Consumer;
-//
-//import com.mojang.datafixers.util.Pair;
-//import com.stevekung.fishofthieves.registry.FOTBiomes;
-//
-//import net.minecraft.core.Registry;
-//import net.minecraft.resources.Identifier;
-//import net.minecraft.resources.ResourceKey;
-//import net.minecraft.world.level.biome.Biome;
-//import net.minecraft.world.level.biome.Climate;
-//
-//import terrablender.api.ParameterUtils;
-//import terrablender.api.Region;
-//import terrablender.api.RegionType;
-//import terrablender.api.VanillaParameterOverlayBuilder;
-//
-//public class TropicalIslandRegion extends Region TODO
-//{
-//    public TropicalIslandRegion(Identifier name, int weight)
-//    {
-//        super(name, RegionType.OVERWORLD, weight);
-//    }
-//
-//    @Override
-//    public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper)
-//    {
-//        var temperature = Climate.Parameter.span(0.2F, 1.0F); // Tropical temperature
-//
-//        // Parameters are almost the same as mushroom islands because we can't generate islands manually
-//        // So replacing the mushroom island biome at the tropical temperature is the best choice
-//        var humidity = Climate.Parameter.span(-1.0F, 1.0F);
-//        var continentalness = Climate.Parameter.span(-1.2F, -0.9F);
-//        var erosion = Climate.Parameter.span(-1.0F, 1.0F);
-//        var weirdness = Climate.Parameter.span(-1.0F, 1.0F);
-//        var depth = Climate.Parameter.point(0.0F);
-//
-//        var builder = new VanillaParameterOverlayBuilder();
-//        new ParameterUtils.ParameterPointListBuilder()
-//                .temperature(temperature)
-//                .humidity(humidity)
-//                .continentalness(continentalness)
-//                .erosion(erosion)
-//                .weirdness(weirdness)
-//                .depth(depth)
-//                .build().forEach(point -> builder.add(point, FOTBiomes.TROPICAL_ISLAND));
-//
-//        builder.build().forEach(mapper);
-//    }
-//}
+package com.stevekung.fishofthieves.compatibility.terrablender;
+
+import java.util.function.Consumer;
+
+import com.mojang.datafixers.util.Pair;
+import com.stevekung.fishofthieves.registry.FOTBiomes;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Climate;
+
+import terrablender.api.ParameterUtils;
+import terrablender.api.Region;
+import terrablender.api.RegionType;
+import terrablender.api.VanillaParameterOverlayBuilder;
+
+public class TropicalIslandRegion extends Region
+{
+    public TropicalIslandRegion(Identifier name, int weight)
+    {
+        super(name, RegionType.OVERWORLD, weight);
+    }
+
+    @Override
+    public void addBiomes(Registry<Biome> registry, Consumer<Pair<Climate.ParameterPoint, ResourceKey<Biome>>> mapper)
+    {
+        var temperature = Climate.Parameter.span(0.2F, 1.0F); // Tropical temperature
+
+        // Parameters are almost the same as mushroom islands because we can't generate islands manually
+        // So replacing the mushroom island biome at the tropical temperature is the best choice
+        var humidity = Climate.Parameter.span(-1.0F, 1.0F);
+        var continentalness = Climate.Parameter.span(-1.2F, -0.9F);
+        var erosion = Climate.Parameter.span(-1.0F, 1.0F);
+        var weirdness = Climate.Parameter.span(-1.0F, 1.0F);
+        var depth = Climate.Parameter.point(0.0F);
+
+        var builder = new VanillaParameterOverlayBuilder();
+        new ParameterUtils.ParameterPointListBuilder()
+                .temperature(temperature)
+                .humidity(humidity)
+                .continentalness(continentalness)
+                .erosion(erosion)
+                .weirdness(weirdness)
+                .depth(depth)
+                .build().forEach(point -> builder.add(point, FOTBiomes.TROPICAL_ISLAND));
+
+        builder.build().forEach(mapper);
+    }
+}
