c0f3f4baf2d32c89d79bb0729a2f4033661e075d
SteveKunG <mccommander_minecraft@hotmail.com>
2026-06-29 18:56:31 +0700

    fix: shoal brightness is no longer get value from the above block

--- common/src/main/java/com/stevekung/fishofthieves/client/renderer/entity/ShoalRenderer.java
+++ common/src/main/java/com/stevekung/fishofthieves/client/renderer/entity/ShoalRenderer.java
@@ -6,6 +6,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.stevekung.fishofthieves.entity.shoal.Shoal;
 import com.stevekung.fishofthieves.mixin.client.accessor.EntityRenderDispatcherAccessor;
 
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
@@ -28,12 +29,15 @@ public class ShoalRenderer extends EntityRenderer<Shoal>
 
     // For petals rotation equations credit to https://github.com/VazkiiMods/Botania/blob/1.20.x/Xplat/src/main/java/vazkii/botania/client/render/block_entity/PetalApothecaryBlockEntityRenderer.java#L63
     // Under the Botania license.
+    @SuppressWarnings("deprecation")
     @Override
     public void render(Shoal shoal, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight)
     {
         poseStack.pushPose();
         var ageInTicks = shoal.tickCount + partialTicks;
         var list = shoal.getShoalFishClient();
+        var level = shoal.level();
+        var blockPos = shoal.blockPosition();
 
         var offsetPerFish = 360F / list.size();
         var modifier = 15F;
@@ -43,6 +47,11 @@ public class ShoalRenderer extends EntityRenderer<Shoal>
         var v = 1F / 8F;
 
         poseStack.translate(-0.05F, 0F, 0F);
+
+        if (level.getBlockState(blockPos.above()).isSolid())
+        {
+            packedLight = LightTexture.pack(this.getBlockLightLevel(shoal, blockPos.below(2)), this.getSkyLightLevel(shoal, blockPos.below(2)));
+        }
 
         for (var index = 0; index < list.size(); index++)
         {
