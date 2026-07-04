573205ec39d1bee7c7fae4935e62d0dd2e64870b
SteveKunG <mccommander_minecraft@hotmail.com>
2026-07-04 15:47:16 +0700

    chore: update deps and fix terrablender

--- common/src/main/java/com/stevekung/fishofthieves/compatibility/terrablender/FOTTerraBlender.java
+++ common/src/main/java/com/stevekung/fishofthieves/compatibility/terrablender/FOTTerraBlender.java
@@ -2,9 +2,6 @@ package com.stevekung.fishofthieves.compatibility.terrablender;
 
 import com.stevekung.fishofthieves.FishOfThieves;
 
-import net.minecraft.core.registries.Registries;
-import net.minecraft.data.registries.VanillaRegistries;
-
 import terrablender.api.Regions;
 import terrablender.api.SurfaceRuleManager;
 
@@ -14,7 +11,6 @@ public class FOTTerraBlender
     {
         Regions.register(new TropicalIslandRegion(FishOfThieves.id("tropical_island"), FishOfThieves.CONFIG.biome.tropicalIslandRegionWeight));
 
-        var biomes = VanillaRegistries.createLookup().lookupOrThrow(Registries.BIOME);
-        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, FishOfThieves.MOD_ID, FOTSurfaceRuleData.overworld(biomes));
+        SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, FishOfThieves.MOD_ID, FOTSurfaceRuleData::overworld);
     }
 }
