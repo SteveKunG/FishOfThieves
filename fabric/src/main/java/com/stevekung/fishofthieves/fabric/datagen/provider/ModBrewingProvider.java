cbbea91b2ccb8761efe9102c32dd78176d9c3006
SteveKunG <mccommander_minecraft@hotmail.com>
2026-08-18 13:10:02 +0700

    fix: potion recipes datagen

--- fabric/src/main/java/com/stevekung/fishofthieves/fabric/datagen/provider/ModBrewingProvider.java
+++ fabric/src/main/java/com/stevekung/fishofthieves/fabric/datagen/provider/ModBrewingProvider.java
@@ -2,11 +2,11 @@ package com.stevekung.fishofthieves.fabric.datagen.provider;
 
 import com.stevekung.fishofthieves.registry.FOTItems;
 
-import net.minecraft.data.recipes.BrewingProvider;
 import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.packs.VanillaBrewingProvider;
 import net.minecraft.world.item.alchemy.Potions;
 
-public class ModBrewingProvider extends BrewingProvider
+public class ModBrewingProvider extends VanillaBrewingProvider
 {
     protected ModBrewingProvider(RecipeOutput recipeOutput)
     {
@@ -16,12 +16,9 @@ public class ModBrewingProvider extends BrewingProvider
     @Override
     protected void buildMixes()
     {
-        this.buildMix(Potions.AWKWARD, FOTItems.PLENTIFIN, Potions.LUCK);
-        this.buildMix(Potions.AWKWARD, FOTItems.ISLEHOPPER, Potions.WATER_BREATHING);
+        this.buildStartMix(FOTItems.PLENTIFIN, Potions.LUCK);
+        this.buildStartMix(FOTItems.ISLEHOPPER, Potions.WATER_BREATHING);
     }
-
-    @Override
-    protected void addContainers() {}
 
     @Override
     protected void addContainerTransformations() {}
