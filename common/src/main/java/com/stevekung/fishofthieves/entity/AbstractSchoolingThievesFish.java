9e2649cd5ef5867805f1f9a9c7253f8ae3f2f0a1
SteveKunG <mccommander_minecraft@hotmail.com>
2026-08-27 22:47:36 +0700

    fix: do not use toList when adding flock followers

--- common/src/main/java/com/stevekung/fishofthieves/entity/AbstractSchoolingThievesFish.java
+++ common/src/main/java/com/stevekung/fishofthieves/entity/AbstractSchoolingThievesFish.java
@@ -1,6 +1,7 @@
 package com.stevekung.fishofthieves.entity;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.jetbrains.annotations.Nullable;
@@ -171,7 +172,8 @@ public abstract class AbstractSchoolingThievesFish<T extends FishData> extends A
     @SuppressWarnings("rawtypes")
     public void addThievesFishFollowers(Stream<AbstractSchoolingThievesFish> followers)
     {
-        var list = followers.limit((long) this.getMaxSchoolSize() - this.getSchoolSize()).filter(fish -> fish != this).toList();
+        // Do not change to toList() because we're adding flock followers below
+        var list = followers.limit((long) this.getMaxSchoolSize() - this.getSchoolSize()).filter(fish -> fish != this).collect(Collectors.toList());
         var hasFlockFollowerMem = this.getBrain().hasMemoryValue(FOTMemoryModuleTypes.FLOCK_FOLLOWERS);
 
         list.forEach(fish ->
